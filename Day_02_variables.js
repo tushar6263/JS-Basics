e2a4fdb9813925f6b9f6ce4fec761f98a15c6b49
tushar626346 <tushar626346@gmail.com>
2026-05-06 08:14:13 +0530

    git

---
+++ Day_02_variables.js
@@ -0,0 +1,24 @@
+const product = 'laptop';
+// const: Block-scoped variable that cannot be updated or redeclared after assignment.
+
+let productid = 123123;
+// let: Block-scoped variable that can be updated but not redeclared in the same scope.
+
+var productprice = 120000;
+// var: Function-scoped variable that can be redeclared and updated.
+
+producttype = 'electronic';
+// js automatic variable bana deta h 
+
+
+console.table([product,productid,productprice,producttype]);
+
+
+// product = 'mobile';
+productid = 345678;
+productprice = 30000;
+producttype = 'qwertyu';
+
+
+
+console.table([product,productid,productprice,producttype]);
