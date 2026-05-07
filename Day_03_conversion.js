b9536d1ed9c95ae2e7ebc0befe8194fd606dbca2
tushar626346 <tushar626346@gmail.com>
2026-05-07 08:14:12 +0530

    Day 2 to learn js (variable)

---
+++ Day_03_conversion.js
@@ -0,0 +1,29 @@
+let matchscore = '100';
+console.log(typeof matchscore);
+
+let numberScore = Number(matchscore);
+console.log(typeof numberScore);
+
+
+const runs = null;
+console.log(typeof runs);
+let stringRuns = String(runs);
+console.log(typeof stringRuns);
+
+
+const name = 'abhishek';
+const nameNumber = Number(name);
+console.log(typeof nameNumber);
+
+
+const bool = '';
+console.log(typeof bool);
+
+const Booleanbool = Boolean(bool);
+console.log(typeof Booleanbool);
+console.log(Booleanbool);
+
+
+// 1--- true    0---- false
+// ' '----false   'tushar' ---- true
+// 3 ---- true
